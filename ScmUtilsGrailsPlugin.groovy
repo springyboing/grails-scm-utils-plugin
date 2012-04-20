b13a4fe43ee90421e5167e8c8a307a43b72cc614
Tim Redding <tim.redding@tribalddb.co.uk>
2012-04-20 18:34:24 +0100

    Branch works for git on windows..

--- ScmUtilsGrailsPlugin.groovy
+++ ScmUtilsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ScmUtilsGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "1.2.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,6 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
 
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
@@ -13,27 +12,18 @@ grails.project.dependency.resolution = {
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
     }
-
     plugins {
         build(":tomcat:$grailsVersion",
-              ":release:1.0.0") {
+                ':release:1.0.0.RC1') {
+            excludes "svn", 'nekohtml'
             export = false
         }
     }
 }
 
-//scmutils.rootDir = '/Users/timredding/Documents/projects/Text-Vault/'
+// Alternative project root so we can find .svn or .git folder to identify SCM.
+// scmutils.rootDir = '/Users/timredding/Documents/projects/Text-Vault/'
